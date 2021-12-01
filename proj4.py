95afa2f3c776283418662daac4c55cd244462258
Aidan Kirk <47363073+aidankirk617@users.noreply.github.com>
2021-11-30 19:09:15 -0500

    Added more formatting, started to count refs

--- proj4.py
+++ proj4.py
@@ -27,6 +27,10 @@ total_mem_ref = 0    # Total memory references
 hit_ratio = 0    # Hit ratio
 miss_ratio = 0    # Miss ratio
 line = 0    # Just a hold for formatting rn
+column = "-------"    # Also for formatting
+
+# Reference Variables
+access = 0
 
 #####----- TABLE BUILDING -----#####
 
@@ -37,8 +41,26 @@ print("\t" + str(set_associative) + " sets total")
 print("\t" + str(set_words) + " words per set\n\n")
 print("Results for Each Reference\n")
 
+# References
+print("%-10s %-10s %-10s %-10s %-10s %-10s %-10s" %("Access", "Address", "  Tag", "Index", "Offset", "Result", "Memrefs"))
+print("%-10s %-10s %-10s %-10s %-10s %-10s %-10s" %(column, column, column, column, column, column, column))
+
+#--- Reading goes here ---
+def memory_check(total_mem_ref):
+    count = 0   # Counter for how many times we do this
+
+    while total_mem_ref < 7:    # Column 1 (Access)
+        # if column 2 == "R":     pseudocode for when we figure out reading
+        #   access = "read"
+        # elif column 2 == "W":
+        #   access = "write"
+
+    # Put in all the data we collected, print it, start all over again until each line is full
+    print("%-10s %-10s %-10s %-10s %-10s %-10s %-10s" %(str(access), "Address", "  Tag", "Index", "Offset", "Result", "Memrefs"))
+    count += 1
+
 # Build Summary
-print("Simulation Summary Statistics\n-----------------------------")
+print("\n\nSimulation Summary Statistics\n-----------------------------")
 print("Total hits\t\t: " + str(total_hits))
 print("Total misses\t\t: " + str(total_misses))
 print("Total accesses\t\t: " + str(total_accesses))
@@ -47,7 +69,7 @@ print("Hit ratio\t\t: " + str(hit_ratio))
 print("Miss ratio\t\t: " + str(miss_ratio))
 
 # Final Data Cache State
-print("\n\tFinal Data Cache State\n------------------------------")
+print("\n\n\tFinal Data Cache State\n------------------------------")
 
 while set < 2:
     print("Set " + str(set))
